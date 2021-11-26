56610162d0a9e88eaa9a0296019746d09b11d309
Bhushan Abhisheki <bhushanabhisheki@gmail.com>
2021-11-26 19:21:53 +0530

    Understanding the Component Selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  //selector: 'app-servers',
+  //selector: '.app-servers', //class selector
+  // selector: '#app-servers', //id  selector
+  selector: '[app-servers]', //attribute  selector
+
   //templateUrl: './servers.component.html',
   template: `<app-server></app-server> <app-server></app-server>`,
   styleUrls: ['./servers.component.scss'],
