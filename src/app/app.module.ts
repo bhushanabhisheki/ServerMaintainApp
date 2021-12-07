d02f834a58add868d9aa8e4bbb0f42fcb71b3917
Bhushan Abhisheki <bhushanabhisheki@gmail.com>
2021-12-07 13:24:35 +0530

    ngFor and ngIf Recap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
+import { DirectivesComponent } from './directives/directives.component';
 
 @NgModule({
-  declarations: [AppComponent, CockpitComponent, ServerElementComponent],
+  declarations: [
+    AppComponent,
+    CockpitComponent,
+    ServerElementComponent,
+    DirectivesComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [DirectivesComponent],
 })
 export class AppModule {}
